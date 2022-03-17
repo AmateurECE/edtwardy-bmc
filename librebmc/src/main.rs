47510c06c612daf518fd9f6c53bc09ecc2241e09
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-03-16 21:18:02 -0500

    Implement most of Redfish ServiceRoot

---
+++ librebmc/src/main.rs
@@ -0,0 +1,102 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            main.rs
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     Entrypoint for the application.
+//
+// CREATED:         02/26/2022
+//
+// LAST EDITED:     03/16/2022
+//
+// Copyright 2022, Ethan D. Twardy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+////
+
+use std::{convert::Infallible, net::SocketAddr, path::PathBuf};
+
+use hyper::{Body, Request, Response, Server, StatusCode};
+// Import the routerify prelude traits.
+use routerify::prelude::*;
+use routerify::{Middleware, Router, RouterService, RequestInfo};
+
+mod redfish;
+
+// A middleware which logs an http request.
+async fn logger(req: Request<Body>) -> Result<Request<Body>, Infallible> {
+    println!("{} {} {}", req.remote_addr(), req.method(), req.uri().path());
+    Ok(req)
+}
+
+// Define an error handler function which will accept the `routerify::Error`
+// and the request information and generates an appropriate response.
+async fn error_handler(err: routerify::RouteError, _: RequestInfo) ->
+    Response<Body>
+{
+    eprintln!("{}", err);
+    Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body(Body::from(format!("Something went wrong: {}", err)))
+        .unwrap()
+}
+
+// Create a `Router<Body, Infallible>` for response body type `hyper::Body`
+// and for handler error type `Infallible`.
+fn router() -> Router<Body, Infallible> {
+    let service_path = "/redfish/v1";
+    let service = redfish::RedfishServiceBuilder::default()
+        .odata_id(PathBuf::from(service_path))
+        .build()
+        .unwrap();
+
+    // Create a router and specify the logger middleware and the handlers.
+    // Here, "Middleware::pre" means we're adding a pre middleware which will
+    // be executed before any route handlers.
+    Router::builder()
+        // Specify the state data which will be available to every route
+        // handlers, error handler and middlewares.
+        .data(service)
+        .middleware(Middleware::pre(logger))
+        .get(service_path, redfish::v1_service_root)
+        .err_handler_with_info(error_handler)
+        .build()
+        .unwrap()
+}
+
+#[tokio::main]
+async fn main() {
+    let router = router();
+
+    // Create a Service from the router above to handle incoming requests.
+    let service = RouterService::new(router).unwrap();
+
+    // The address on which the server will be listening.
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // Create a server by passing the created service to `.serve` method.
+    let server = Server::bind(&addr).serve(service);
+
+    println!("App is running on: {}", addr);
+    if let Err(err) = server.await {
+        eprintln!("Server error: {}", err);
+   }
+}
+
+///////////////////////////////////////////////////////////////////////////////
---
+++ redfish/src/server.rs
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            server.rs
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     Traits for implementing a Redfish server.
+//
+// CREATED:         02/27/2022
+//
+// LAST EDITED:     02/27/2022
+//
+// Copyright 2022, Ethan D. Twardy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+////
+
+// A Redfish Error
+pub struct RedfishError;
+
+// A uniquely addressable system
+pub trait System {
+    fn get_id(&self) -> u64;
+}
+
+// The ComputerSystem trait implements a subset of the functionality here:
+// http://redfish.dmtf.org/schemas/v1/ComputerSystem.v1_17_0.yaml
+pub trait ComputerSystem: System {
+    fn reset(&self) -> Result<(), RedfishError>;
+}
+
+pub struct Service {
+    systems: Vec<Box<dyn System>>,
+}
+
+///////////////////////////////////////////////////////////////////////////////
