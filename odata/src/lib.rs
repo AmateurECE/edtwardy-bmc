14e1a86f498bb9e25b26d2d24b598b5969751e03
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-04-03 14:19:29 -0500

    Source reorganization for odata crate

--- odata/src/lib.rs
+++ odata/src/lib.rs
@@ -3,27 +3,50 @@
 //
 // AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
 //
-// DESCRIPTION:     Entrypoints for the odata crate.
+// DESCRIPTION:     The odata crate provides types and traits for working with
+//                  OData resource in an OpenData RESTful API.
 //
 // CREATED:         04/01/2022
 //
-// LAST EDITED:     04/01/2022
+// LAST EDITED:     04/03/2022
+//
+// Copyright 2022, Ethan D. Twardy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
 ////
 
 use std::path::PathBuf;
-
 use serde::{self, ser::SerializeStruct};
 
-pub trait ResourceMetadata {
-    const ODATA_TYPE: &'static str;
-}
-
+// Must be implemented by traits that can be wrapped with Resource<>
+pub trait ResourceMetadata { const ODATA_TYPE: &'static str; }
 pub trait Serialize {
     const CARDINALITY: usize;
     fn serialize<S: serde::ser::SerializeStruct>(&self, serializer: &mut S) ->
         Result<(), S::Error>;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// Resource
+////
+
+// Resource Wrapper. Provides OData metadata for any type.
 pub struct Resource<T: Serialize + ResourceMetadata> {
     resource: T,
     odata_id: PathBuf,
@@ -50,7 +73,7 @@ impl<T: Serialize + ResourceMetadata> serde::Serialize for Resource<T> {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Test
+// Resource Test
 ////
 
 #[cfg(test)]
@@ -58,34 +81,32 @@ mod tests {
     use std::path::PathBuf;
     use crate::{Resource, ResourceMetadata, Serialize};
     use serde::ser::SerializeStruct;
+    use serde_json;
 
-    struct Person {
-        name: String,
-    }
-
+    struct Person (String);
     impl ResourceMetadata for Person {
-        const ODATA_ID: &'static str = "#Person";
+        const ODATA_TYPE: &'static str = "#Person";
     }
 
     impl Serialize for Person {
         const CARDINALITY: usize = 1;
         fn serialize<S: SerializeStruct>(&self, serializer: &mut S) ->
             Result<(), S::Error>
-        {
-            serializer.serialize_field("Name", &self.name)
-        }
+        { serializer.serialize_field("Name", &self.0) }
     }
 
     #[test]
-    fn it_works() {
-        let chuck = Person { name: "Chuck".to_string() };
-        let resource: Resource<Person> = Resource {
-            odata_id: PathBuf::from("/Chuck"),
-            odata_type: PERSON_TYPE,
-            resource: chuck,
-        };
-
-        assert!(serde_json::to_string(&resource).is_ok())
+    fn serialize_correctness() {
+        let resource: Resource<Person> = Resource::new(
+            PathBuf::from("/Chuck"),
+            Person("Chuck".to_string()));
+        let result = serde_json::to_string(&resource);
+        assert!(result.is_ok());
+        assert_eq!(
+            "{\"@odata.id\":\"/Chuck\",".to_string()
+                + "\"@odata.type\":\"#Person\",\"Name\":\"Chuck\"}",
+            result.unwrap()
+        )
     }
 }
 
